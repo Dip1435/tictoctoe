2182c0f75f9ea4162ffb5aa114ba1c60708eb293
Dip1435 <127203353+Dip1435@users.noreply.github.com>
2023-08-27 10:26:54 +0530

    add my project files
    this is code for my game tictoctoe

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './TicTocToe-Game/Board';
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
